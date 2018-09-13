25ae4c4e40e0c9693b60c879cecaeecfde0f094a
NasibNia <nasibnia@gmail.com>
2018-09-12 23:15:47 -0700

    controllers & routes

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,49 @@
+var burger = require ("../models/burger.js");
+var express= require("express");
+var router = express.Router();
+
+//get all the burgers
+router.get("/" , function(req,res){
+    burger.selectAll(function(data){
+        res.render("index" , {allBurgers : data});
+    });
+
+});
+
+//insert a new burger
+router.post("/api/butger/" , function(req,res){
+
+    burger.insertOne({
+        burger_name : req.body.burger_name,
+        devoured    :  req.body.devoured
+    }, function(data){
+        // sends back the id of new inserted object into data base
+        res.json({id : data.insertId});
+    });  
+});
+
+//update a new burger
+router.put("/api/burger/:id", function(req,res){
+
+    var id = req.params.id;
+    
+    var condition = "WHERE id="+id;
+
+    burger.updateOne({
+        burger_name : req.body.burger_name,
+        devoured    : req.body.devoured
+        }, condition , function(data){
+            if (data.changedRows === 0) {
+                // If no rows were changed, then the ID must not exist, so 404
+                return res.status(404).end();
+              } else {
+                res.status(200).end();
+              }
+        }
+    );
+
+});
+
+
+
+module.exports = router;
